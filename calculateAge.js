7ebbf5d2914120c73085463f3e63917eb6256e94
Alantiel Freire Marins <amarins@uolinc.com>
2023-02-02 14:17:01 -0300

    Extraindo templates do arquivo principal

---
+++ calculateAge.js
@@ -0,0 +1,5 @@
+module.exports = function calculateAge(birthday) {
+  const ageDifMs = Date.now() - birthday.getTime();
+  const ageDate = new Date(ageDifMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
